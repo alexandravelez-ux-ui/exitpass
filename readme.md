bba2ad03b0910c593f93ad1884005ecc2c560c71
Alexandra Velez <alexandravelez@Alexandras-MacBook-Pro.local>
2021-04-12 20:45:24 -0500

    got sidebar transition going

--- readme.md
+++ readme.md
@@ -1,10 +1,6 @@
 What's next?
-- Finish Making Responsive
--- Continue Sidebar Responses (https://codepen.io/husnimun/pen/pJvEeL)
-- Create Input Side View
-- Hamburger menu icon adjustments (transparency)
-- Reverse hamburger animation
-- isClose class, explicityly closed 
+-Fix sidebar transition
+-Finish reverse hamburger menu animation 
 
 Figma App Design
 -..
--- src/Recipient/Sidebar.js
+++ src/Recipient/Sidebar.js
@@ -1,13 +1,20 @@
 import React, { useState, useEffect } from "react";
 import "./style.css";
 import HamburgerMenu from "./HamburgerMenu";
+import classSet from "react-classset";
+
 
 export default function Sidebar(props) {
   const [isOpen, setIsOpen] = useState(false);
+  const containerClassSet = classSet({
+    sidebarContainer: true,
+    "font-weight-bold": true,
+    closed: !isOpen, 
+  })
   return (
     <div>
       <HamburgerMenu isOpen={isOpen} setIsOpen={setIsOpen} />
-      <div className="sidebarContainer font-weight-bold">
+      <div className={containerClassSet}>
         <div className={isOpen ? "sidebarContent" : "hidden sidebarContent"}>
           <img className="logo" src="https://i.imgur.com/o9Py1iT.png" />
           {Object.entries(props.categories).map(([table, name]) => (
