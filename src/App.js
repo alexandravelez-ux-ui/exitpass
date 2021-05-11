8c0dc6a367e1c303a98e9899d2964cc3724d489b
Alexandra Velez <alexandravelez@Alexandras-MacBook-Pro.local>
2021-05-10 20:07:45 -0500

    Chakra

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { useState, useEffect } from "react";
 import "./style.css";
 import Recipient from "./Recipient";
+import { ChakraProvider } from "@chakra-ui/react"
+
+const theme = extendTheme ({
+  colors: {
+    red: "",
+    brightRed: "",
+    white: "#fff"
+  }
+}) 
 
 export default function App() {
   return (
-    <div className="jost-font">
-      <Recipient />
-    </div>
+    <ChakraProvider>
+      <div className="jost-font">
+        <Recipient />
+      </div>
+    </ChakraProvider>
   );
 }
