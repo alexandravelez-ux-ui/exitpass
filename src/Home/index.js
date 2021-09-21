563653716ecf58edb0513c384a7ad05a343fdc8f
Alexandra Velez <alexandravelez@Alexandras-MacBook-Pro.local>
2021-09-20 20:44:27 -0500

    Added content to landing page

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,24 +1,57 @@
-import react from "react" 
-import { Flex, Circle, Image, Text, Button, chakra} from "@chakra-ui/react"
-import TabSection from "./tabsSection"
+import react from "react";
+import { Flex, Circle, Image, Text, Button, chakra } from "@chakra-ui/react";
+import TabSection from "./tabsSection";
 export default function () {
-    return (
-        <Flex fontFamily="Jost" align="center" paddingTop="30px" minHeight="100vh" direction="column">
-            <Flex align="center" justify="space-around" width="100vw" direction="row">
-                <Image background="red" width="200px" height="100px"/>
-                <Text>Why ExitPass</Text>
-                <Text>How it Works</Text>
-                <Text>Pricing</Text>
-                <Text>Get Started</Text>
-            </Flex>
-            <Flex direction="row" width="60vw" marginTop="80px">
-                <Image flex="1" background="gray"/>
-                <Flex  flex="1" direction="column" justify="center" marginLeft="75px">
-                    <Text>Headline</Text>
-                    <Text marginTop="32px">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Text>
-                </Flex>
-            </Flex>
-            <TabSection/>
+  return (
+    <Flex
+      fontFamily="Jost"
+      align="center"
+      paddingTop="30px"
+      minHeight="100vh"
+      direction="column"
+    >
+      <Flex align="center" justify="space-around" width="100vw" direction="row">
+        <Image background="red" width="200px" height="100px" />
+        <Text>Why ExitPass</Text>
+        <Text>How it Works</Text>
+        <Text>Pricing</Text>
+        <Text>Get Started</Text>
+      </Flex>
+      <Flex direction="row" width="60vw" marginTop="80px">
+        <Image flex="1" background="gray" />
+        <Flex flex="1" direction="column" justify="center" marginLeft="75px">
+          <Text>What is ExitPass</Text>
+          <Text marginTop="32px">
+            Exitpass is a tool for preserving your digital accounts for your
+            chosen beneficiaries after death. We ensure that your digital
+            identity is passed on to people you trust.
+          </Text>
         </Flex>
-    )
+      </Flex>
+      <TabSection />
+      <Flex direction="column" width="60vw" align="center" marginTop="80px">
+        <Text>How ExitPass Works</Text>
+        <Flex
+          flex="1"
+          direction="row"
+          justify="space-between"
+          marginRight="75px"
+        >
+          <Text marginTop="32px" flex="1" padding="16px">
+            When using ExitPass, you enter your online account details just like
+            a password manager and then specify how you want the account handled
+            in the event of your death.
+          </Text>
+          <Text marginTop="32px" flex="1" padding="16px">
+            You then specify who will be able to access the account to carry out
+            these wishes.
+          </Text>
+          <Text marginTop="32px" flex="1" padding="16px">
+            They will be invited to create a beneficiary account where they will
+            be able to access the account data after your death.
+          </Text>
+        </Flex>
+      </Flex>
+    </Flex>
+  );
 }
--- src/Home/tabsSection.js
+++ src/Home/tabsSection.js
@@ -1,16 +1,104 @@
-import react, {useState} from "react"
-import { Flex, Circle, Image, Text, Button, chakra} from "@chakra-ui/react"
+import react, { useState } from "react";
+import {
+  Flex,
+  Circle,
+  Image,
+  Text,
+  Button,
+  chakra,
+  Img,
+  Box,
+} from "@chakra-ui/react";
+import confident from "./confident.jpg";
+import flexibility from "./flexibility.jpg";
+import intentions from "./intentions.jpg";
+import security from "./security.jpg";
+
+const images = [security, flexibility, intentions, confident];
 
 export default function () {
-    return (
-        <Flex background="gray" direction="row" width="60vw" marginTop="80px" padding="32px">
-            <Flex  flex="1" direction="column" align="center" justify="space-around" color="white">
-                <Text background="blue" width="80%" padding="5px">Headline</Text>
-                <Text background="blue" width="80%" padding="5px">Headline</Text>
-                <Text background="blue" width="80%" padding="5px">Headline</Text>
-                <Text background="blue" width="80%" padding="5px">Headline</Text>
-            </Flex>
-            <Image flex="1" background="red"/>
-        </Flex>
-    )
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  return (
+    <Flex
+      height="30vh"
+      background="gray"
+      direction="row"
+      width="60vw"
+      marginTop="80px"
+      padding="32px"
+    >
+      <Flex flex="1" direction="column" justify="space-between" color="white">
+        <Box
+          onMouseOver={() => setCurrentImageIndex(0)}
+          background="blue"
+          width="80%"
+          padding="5px"
+        >
+          <Text fontSize="24px" fontWeight="bold">
+            Security
+          </Text>
+          {currentImageIndex == 0 ? (
+            <Text>
+              We use the latest encryption technology to secure and protect your
+              data
+            </Text>
+          ) : (
+            ""
+          )}
+        </Box>
+        <Box
+          onMouseOver={() => setCurrentImageIndex(1)}
+          background="blue"
+          width="80%"
+          padding="5px"
+        >
+          <Text fontSize="24px" fontWeight="bold">
+            Flexibility
+          </Text>
+          {currentImageIndex == 1 ? (
+            <Text>
+              Whether its your bank account or social media account, we will
+              safeguard your data and carry out your wishes
+            </Text>
+          ) : (
+            ""
+          )}
+        </Box>
+        <Box
+          onMouseOver={() => setCurrentImageIndex(2)}
+          background="blue"
+          width="80%"
+          padding="5px"
+        >
+          <Text fontSize="24px" fontWeight="bold">
+            Intention
+          </Text>
+          {currentImageIndex == 2 ? (
+            <Text>Choose who will gain access to your digital accounts</Text>
+          ) : (
+            ""
+          )}
+        </Box>
+        <Box
+          onMouseOver={() => setCurrentImageIndex(3)}
+          background="blue"
+          width="80%"
+          padding="5px"
+        >
+          <Text fontSize="24px" fontWeight="bold">
+            Confidence
+          </Text>
+          {currentImageIndex == 3 ? (
+            <Text>
+              Have the peace of mind that your digital accounts will be well
+              taken care of
+            </Text>
+          ) : (
+            ""
+          )}
+        </Box>
+      </Flex>
+      <Image src={images[currentImageIndex]} />
+    </Flex>
+  );
 }
